7e09bef466e026a93f9cb976d7d13b6c59fa0239
GianniniMatteo <matteogiannini294@gmail.com>
2024-10-29 17:45:12 +0100

    Update build file

--- 00-qa/src/main/java/IT/UNIBO/qa/cat.java
+++ 00-qa/src/main/java/IT/UNIBO/qa/cat.java
@@ -1,12 +1,7 @@
-package IT.UNIBO.qa;
+package it.unibo.qa;
 
-public class cat {
+public class Cat {
 //this class describes a cat
-
-    
-
-
-
 public String Name, Color, Breed;
 public int Age;
 
@@ -14,9 +9,7 @@ public int Age;
 private static int  NINE = 9;
 static public  int numberoflives= NINE;
 
-public cat(String meow,
-final String c,final int age,
-final String korat) {
+public Cat(String meow, final String c,final int age, final String korat) {
     this.Name =meow ;
     Color=c  ;
     this.Age=age;
@@ -24,54 +17,38 @@ final String korat) {
 }
 
 // This method is useful because it allows the cat to meow and introduce itself to the world in a single line of code.
-    public String
-    Meow()
-    {return "Meow, meow, I am " + Name + ", hear me purr, In this feline world, I'm the star, that's for sure. With whiskers long and fur so sleek, I'm here to share my story, so take a peek!";
-}public
+    public String Meow(){
+        return "Meow, meow, I am " + Name + ", hear me purr, In this feline world, I'm the star, that's for sure. With whiskers long and fur so sleek, I'm here to share my story, so take a peek!";
+}
 
-
-
-void setcolor(String c) {
+public void setcolor(String c){
     this.Color = 
     c;
 }
 
-    public String GET_COLOR() {
-return this.Color;
-    }
+public String GET_COLOR(){
+    return this.Color;
+}
 
-    public int hasSameColor( cat  cat )
-    {
-if (Color==cat.Color) {return   1;}else{return 0;}
-    }
+public int hasSameColor( Cat  cat ){
+    if (Color==cat.Color) {return   1;}else{return 0;}
+}
 
 String name( )
 {
     return Name;
 }
 
-
-String BREED( )
-
-{
-    return 
-    this.Breed;
+String BREED( ){
+    return this.Breed;
 }
 
-public
-static
-void
-main(
-    String[] ARGS
-) {
-    cat cat = new cat(
+public static void main(String[] ARGS) {
+    Cat cat = new Cat(
         "Kiri",
         "Gray",
-        4, "Kor"+
-         "at");
+        4, "Kor"+"at");
 
-    System.
-    out.
-    println(cat.Meow());
+    System.out.println(cat.Meow());}
+    
 }
-        }
