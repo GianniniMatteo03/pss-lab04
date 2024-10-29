a5b76be40b4ef092e10aabb7066fc8244cfc15f4
GianniniMatteo <matteogiannini294@gmail.com>
2024-10-29 16:39:10 +0100

    Update build file

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/TestAllClasses.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/TestAllClasses.java
@@ -3,14 +3,24 @@ package it.unibo.encapsulation;
 public class TestAllClasses {
     public static void main(final String[] args) {
         final Calculator calc = new Calculator();
-        System.out.println("1+2=" + calc.add(1, 2));
+
+        System.out.println(calc.getOperationsPerformed()); //get per testarlo in terminale
+        System.out.println(calc.getLastResult()); //get per testarlo in terminale
         Calculator.printCalculatorStatus(calc);
         System.out.println("-1-(+2)=" + calc.sub(-1, 2));
+        System.out.println("");
+
+        System.out.println(calc.getOperationsPerformed());
+        System.out.println(calc.getLastResult());
         Calculator.printCalculatorStatus(calc);
         System.out.println("8*3=" + calc.mul(8, 3));
+        System.out.println("");
+        
+        System.out.println(calc.getOperationsPerformed());
+        System.out.println(calc.getLastResult());
         Calculator.printCalculatorStatus(calc);
         System.out.println("8/4=" + calc.div(8, 4));
-        Calculator.printCalculatorStatus(calc);
+        System.out.println("");
 
         final Smartphone htcOne = new Smartphone("HTC", "One", 1024);
         final Smartphone note3 = new Smartphone(4, 2048, 8192, "Samsung", "Galaxy Note 3", true, true, true);
